dc6c68d96782d6bf3294d14e3b12c978e4d72e3f
Abiola-Sodiq <127953432+Abiola-Sodiq@users.noreply.github.com>
2023-06-14 15:07:30 +0100

    Update Input.jsx

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Input = () => {
+const Input = (props) => {
   return (
     <div>
-        <input type="text" id='input' />
+        <input type="text" className=' rounded-md w-60 h-10 border-none bg-gray-200' onChange={props.newName} />
     </div>
   )
 }
